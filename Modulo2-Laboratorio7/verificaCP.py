4946e6ec3999f5bcb8c62dc4048f1a49b693a568
Rodrigo Araújo <rodrigobragavasco@gmail.com>
2021-02-01 18:40:56 -0300

    atualizando comentários do lab7

--- Modulo2-Laboratorio7/verificaCP.py
+++ Modulo2-Laboratorio7/verificaCP.py
@@ -3,23 +3,23 @@ class Monitor:
 		self.numElementos = 0
 
 	def consumidorBloqueado(self,id):
-	    '''Recebe o id do leitor. Verifica se a decisao de bloqueio esta correta.'''
+	    '''Recebe o id do consumidor. Verifica se a decisao de bloqueio esta correta.'''
 	    if(self.numElementos != 0):
 		    print("ERRO: consumidor " + str(id) + " bloqueado quando existe elementos para consumir!")
 
 	def produtorBloqueado(self,id):
-	    '''Recebe o id do escritor. Verifica se a decisao de bloqueio esta correta.'''
+	    '''Recebe o id do produtor. Verifica se a decisao de bloqueio esta correta.'''
 	    if(self.numElementos < 10):
 		    print("ERRO: produtor " + str(id) + " bloqueado quando ha espaco para inserir elemento!")
 
 	def consumidorRetirandoElemento(self,id):
-		'''Recebe o id do leitor, verifica se pode ler e registra que esta lendo.'''
+		'''Recebe o id do consumidor, verifica se pode remover algum elemento e informa que foi removido.'''
 		if(self.numElementos == 0):
 			print("ERRO: consumidor " + str(id) + " esta retirando elemento quando nao existe nenhum elemento!")
 		self.numElementos-=1
 
 	def produtorInserindoElemento(self,id):
-		'''Recebe o id do escritor, verifica se pode escrever e registra que esta escrevendo'''
+		'''Recebe o id do produtor, verifica se pode inserir um elemento e informa que tem um elemento a mais'''
 		if(self.numElementos > 9):
 			print("ERRO: produtor " + str(id) + " esta inserindo elemento quando nao existe espaco!")
 		self.numElementos+=1
